173d61058ed4fcf8b202f69b18aca026c06eedfe
Esteban Márquez Delgado <emarquez1895@gmail.com>
2022-11-21 14:18:39 -0600

    nb Intro and Indicator vs

--- visualizations.py
+++ visualizations.py
@@ -23,7 +23,37 @@ pd.set_option('display.expand_frame_rep', True)
 pd.set_option('display.width', None)
 
 
+def plotly_graph(x, y1, y2, name1, name2, x_label, y_label, title):
+    """
+    Function that plots a line+marker graph with plotly.
 
+        Parameters
+        ----------
+        x: Values Dataframe to be plotted as index with plotly. #data_raw['DateTime']
+        y1: Values of reports results (Ac, Pr, Est). #data_raw['Actual']
+        y2: Values of reports results (Ac, Pr, Est). #data_raw['Consensus']
+        name1: Label of y1 trace (str). #data_raw.columns[1]
+        name2: Label of y2 trace (str). #data_raw.columns[2]
+        x_label: xlabel for plot (str). #data_raw.columns[0]
+        y_label: y_label for plot (str). # "Wk. thousands"        
+        title: Title of the plot (str). "Initial Jobless Claims"
+
+        Returns
+        -------
+        Returns 2 traces (y1,y2) in a didactic graph with plotly for x.
+    """
+
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y1, mode='lines+markers',
+    name=name1, line=dict(color='black'), marker=dict(symbol=2, color='blue')))
+    fig.add_trace(go.Scatter(x=x, y=y2, mode='lines+markers',
+    name=name2, line=dict(color='black'), marker=dict(symbol=2, color='green')))
+    fig.update_layout(title=title, xaxis_title=x_label, yaxis_title=y_label)
+    fig.update_xaxes(showspikes=True)
+    fig.update_yaxes(showspikes=True)
+
+    return fig.show()
 
 
     
